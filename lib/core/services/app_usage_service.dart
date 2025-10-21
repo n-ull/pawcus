93db1b438eedcbf9b8d62845d0d30e831dccd08e
Nicolas Carol <felix.greed@gmail.com>
2025-10-21 11:47:37 -0300

    feat: filter system app usage

--- lib/core/services/app_usage_service.dart
+++ lib/core/services/app_usage_service.dart
@@ -2,14 +2,35 @@ import 'package:app_usage/app_usage.dart';
 import 'package:pawcus/core/exceptions/app_usage_exceptions.dart';
 
 class AppUsageService {
-  static Future<List<AppUsageInfo>> getAppsUsage(
+  Future<List<AppUsageInfo>> getAppsUsage(
     DateTime startPeriod,
     DateTime endPeriod,
   ) async {
     try {
-      return await AppUsage().getAppUsage(startPeriod, endPeriod);
+      final apps = await AppUsage().getAppUsage(startPeriod, endPeriod);
+      final filterApps = apps
+          .where((app) => !isSystemApp(app.packageName))
+          .toList();
+      return filterApps;
     } catch (e) {
-      throw GetAppUsageException('Unexpected error retrieving app usage: ${e.toString()}');
+      throw GetAppUsageException(
+        'Unexpected error retrieving app usage: ${e.toString()}',
+      );
     }
+  }
+
+  bool isSystemApp(String packageName) {
+    final systemPrefixes = [
+      'com.android.',
+      'com.google.android.',
+      'com.samsung.',
+      'com.miui.',
+      'com.huawei.',
+      'com.oppo.',
+      'com.vivo.',
+      'com.motorola.',
+    ];
+
+    return systemPrefixes.any((prefix) => packageName.startsWith(prefix));
   }
 }
