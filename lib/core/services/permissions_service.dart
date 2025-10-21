0bf6eb0635cca1bf44c42d7c7597076f694856ad
Nicolas Carol <felix.greed@gmail.com>
2025-10-21 11:25:52 -0300

    feat: docstrings

--- lib/core/services/permissions_service.dart
+++ lib/core/services/permissions_service.dart
@@ -4,9 +4,11 @@ import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_overlay_window/flutter_overlay_window.dart';
 
+/// A service that handles requesting and checking for permissions.
 class PermissionsService {
   static const _channel = MethodChannel('com.scovillestudios.pawcus/permissions');
 
+  /// Checks if the user has granted usage access permissions.
   Future<bool> hasUsageAccess() async {
     if (!Platform.isAndroid) return true;
     try {
@@ -17,11 +19,13 @@ class PermissionsService {
     }
   }
 
+  /// Checks if the user has granted overlay permissions.
   Future<bool> hasOverlayPermission() async {
     if (!Platform.isAndroid) return true;
     return await FlutterOverlayWindow.isPermissionGranted();
   }
 
+  /// Requests app usage permissions from the user.
   Future<void> requestAppUsagePermissions() async {
     const intent = AndroidIntent(
       action: 'android.settings.USAGE_ACCESS_SETTINGS',
