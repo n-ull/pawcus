bb9368df81d0ff941ef25704da2ea6a5a7914b13
Nicolas Carol <felix.greed@gmail.com>
2025-10-22 11:58:43 -0300

    feat: add more pet details in the home screen for testing

--- lib/features/home.dart
+++ lib/features/home.dart
@@ -1,7 +1,10 @@
+import 'package:app_usage/app_usage.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router_plus/go_router_plus.dart';
+import 'package:pawcus/core/models/app_usage_entry.dart';
 import 'package:pawcus/core/models/pet.dart';
 import 'package:pawcus/core/router/routes.dart';
+import 'package:pawcus/core/services/app_usage_service.dart';
 import 'package:pawcus/core/services/pet_service.dart';
 import 'package:pawcus/core/services/service_locator.dart';
 
@@ -14,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final pet = sl<PetService>().pet;
+  late List<AppUsageEntry> apps;
 
   @override
   void initState() {
@@ -34,11 +38,43 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             ValueListenableBuilder(
               valueListenable: pet,
-              builder: (context, value, child) => Text(value.name),
+              builder: (context, value, child) => Column(
+                children: [
+                  Text(value.name),
+                  LabeledText(value.lastUpdate.toString(), 'Last Update: '),
+                  LabeledText(
+                    value.petStat.happiness.toString(),
+                    'Happiness: ',
+                  ),
+                  LabeledText(value.petStat.energy.toString(), 'Energy: '),
+                  LabeledText(value.petStat.hunger.toString(), 'Hunger: '),
+                  LabeledText(value.petStat.thirst.toString(), 'Thirst: '),
+                  LabeledText(value.petStat.sleep.toString(), 'Sleep: '),
+                  LabeledText(value.petStat.hygiene.toString(), 'Hygiene: '),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                sl<PetService>().checkDailyAppUsage();
+              },
+              child: Text('Check Daily App Usage'),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+//customized text with label
+class LabeledText extends StatelessWidget {
+  final String data;
+  final String label;
+  const LabeledText(this.data, this.label, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [Text(label), Text(data)]);
   }
 }
