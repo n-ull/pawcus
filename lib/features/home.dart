857bb076fc3e01adfcbce9a5b3bd9946561c2001
Nicolas Carol <felix.greed@gmail.com>
2025-10-24 18:42:57 -0300

    feat: integrate pet stats display with ValueListenableBuilder and enhance UI components

--- lib/features/home.dart
+++ lib/features/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pawcus/core/models/app_usage_entry.dart';
+import 'package:pawcus/core/models/pet.dart';
 import 'package:pawcus/core/services/pet_service.dart';
 import 'package:pawcus/core/services/service_locator.dart';
 
@@ -43,53 +44,71 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             children: [
               Text(pet.value.name, style: TextTheme.of(context).titleLarge),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton.filled(
-                    icon: Icon(Icons.bolt_rounded),
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(Colors.green),
-                    ),
-                  ),
-                  IconButton.filled(
-                    icon: Icon(Icons.lunch_dining_rounded),
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(Colors.green),
-                    ),
-                  ),
-                  IconButton.filled(
-                    icon: Icon(Icons.emoji_emotions_rounded),
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(Colors.green),
-                    ),
-                  ),
-                  IconButton.filled(
-                    icon: Icon(Icons.bedtime),
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(Colors.green),
-                    ),
-                  ),
-                ],
+              ValueListenableBuilder<Pet>(
+                valueListenable: pet,
+                builder: (context, currentPet, child) {
+                  return StatsRow(pet: currentPet);
+                },
               ),
               SizedBox(height: 16),
-              Expanded(child: Center(child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.all(Radius.circular(100))
+              Expanded(
+                child: Center(
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.all(Radius.circular(100)),
+                    ),
+                  ),
                 ),
-              ))),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  sl<PetService>().checkDailyAppUsage();
+                },
+                child: Text('Check Use Access'),
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class StatsRow extends StatelessWidget {
+  const StatsRow({super.key, required this.pet});
+
+  final Pet pet;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        StatBubble(
+          data: pet.petStat.energy,
+          icon: Icons.bolt_rounded,
+          label: 'Energy',
+        ),
+        StatBubble(
+          data: pet.petStat.hunger,
+          icon: Icons.lunch_dining_rounded,
+          label: 'Hunger',
+        ),
+        StatBubble(
+          data: pet.petStat.happiness,
+          icon: Icons.emoji_emotions_rounded,
+          label: 'Energy',
+        ),
+        StatBubble(
+          label: 'Sleep',
+          data: pet.petStat.sleep,
+          icon: Icons.nightlight_round_rounded,
+        ),
+      ],
     );
   }
 }
@@ -103,5 +122,45 @@ class LabeledText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(children: [Text(label), Text(data)]);
+  }
+}
+
+class StatBubble extends StatelessWidget {
+  final String label;
+  final double data;
+  final IconData icon;
+
+  Color calculateColor() {
+    if (data < 0.3) {
+      return Colors.red;
+    } else if (data < 0.7) {
+      return Colors.orange;
+    } else {
+      return Colors.green;
+    }
+  }
+
+  const StatBubble({
+    super.key,
+    required this.label,
+    required this.data,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        CircularProgressIndicator(
+          value: data,
+          strokeWidth: 5.0,
+          valueColor: AlwaysStoppedAnimation<Color>(calculateColor()),
+          backgroundColor: Colors.grey[200],
+        ),
+
+        Icon(icon, color: calculateColor(), size: 30),
+      ],
+    );
   }
 }
