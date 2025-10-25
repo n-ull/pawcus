863a30aae08c04b27e02c17e5d8ed174ae6ba143
Nicolas Carol <felix.greed@gmail.com>
2025-10-25 15:48:20 -0300

    feat: cache service and client with dependencies using hive

---
+++ lib/core/services/cache/cache_service.dart
@@ -0,0 +1,8 @@
+import 'package:pawcus/core/services/cache/cache_client.dart';
+
+class CacheService {
+  final CacheClient _cacheClient;
+
+  CacheService(this._cacheClient);
+
+}
--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,7 +1,11 @@
 import 'dart:developer';
 
 import 'package:get_it/get_it.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pawcus/core/services/app_usage_service.dart';
+import 'package:pawcus/core/services/cache/cache_client.dart';
+import 'package:pawcus/core/services/cache/cache_service.dart';
 import 'package:pawcus/core/services/permissions_service.dart';
 import 'package:pawcus/core/services/pet_service.dart';
 
@@ -11,6 +15,20 @@ Future<void> setupServiceLocator() async {
   // Load Dependencies
   sl.registerSingleton(AppUsageService());
   sl.registerSingleton(PermissionsService());
+  
+  // Init CacheService
+  sl.registerSingletonAsync<CacheService>(() async {
+    // initialize hive
+    final dir = await getApplicationDocumentsDirectory();
+    await Hive.initFlutter(dir.path);
+    final box = await Hive.openBox('cache');
+
+    // make the cacheclient and service
+    final cacheClient = CacheClient(box);
+    final svc = CacheService(cacheClient);
+
+    return svc;
+  });
 
   // Init PetService
   sl.registerSingletonAsync<PetService>(() async {
