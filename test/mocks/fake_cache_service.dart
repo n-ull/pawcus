a9b524df5c3884997853baa86fc429fd188f14be
Nicolas Carol <felix.greed@gmail.com>
2025-10-27 10:21:40 -0300

    fix: removed old test integration

--- test/mocks/fake_cache_service.dart
+++ test/mocks/fake_cache_service.dart
@@ -1,36 +1,32 @@
-import 'package:pawcus/core/services/cache/cache_client.dart';
 import 'package:pawcus/core/services/cache/cache_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// A fake cache service that uses an in-memory Map and does not depend on
 /// Hive. This allows tests to run without external mocking libraries.
 class FakeCacheService extends CacheService {
   final FakeCacheClient client;
 
-  FakeCacheService(this.client) : super();
+  FakeCacheService(this.client) : super(client as SharedPreferences);
 }
 
-class FakeCacheClient extends CacheClient {
+class FakeCacheClient {
   final Map<String, dynamic> _store = {};
 
   FakeCacheClient() : super();
 
-  @override
   Future<void> set(String key, dynamic value) async {
     _store[key] = value;
   }
 
-  @override
   T? get<T>(String key, {T? defaultValue}) {
     if (!_store.containsKey(key)) return defaultValue;
     return _store[key] as T?;
   }
 
-  @override
   Future<void> delete(String key) async {
     _store.remove(key);
   }
 
-  @override
   Future<void> clear() async {
     _store.clear();
   }
--- test/services/cache_service_test.dart
+++ test/services/cache_service_test.dart
@@ -1,18 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pawcus/core/models/pet.dart';
 import 'package:pawcus/core/models/pet_stats.dart';
-import 'package:pawcus/core/services/cache/cache_service.dart';
-import '../mocks/fake_cache_service.dart';
 
 void main() {
-  late FakeCacheClient fakeClient;
-  late CacheService cacheService;
 
   setUp(() {
-    fakeClient = FakeCacheClient();
-    cacheService = FakeCacheService(fakeClient);
   });
 
+  // ignore: unused_element
   Pet makeTestPet() => Pet(
         id: 'p-1',
         name: 'Erbcito',
